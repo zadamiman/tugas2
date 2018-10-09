87048221871e7ba15a485ad9c9552be5723835de
ASUS <ASUS@DESKTOP-G258SE4>
2018-10-09 14:09:21 +0700

    java class

---
+++ scr1/src/scr1/scr1_1.java
@@ -0,0 +1,32 @@
+package scr1;
+
+import java.util.Scanner;
+
+public class scr1_1 {
+
+   
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        int rata;
+        
+        Scanner dataln = new Scanner(System.in);
+        
+        System.out.print("Masukkan data 1 : ");
+        a = dataln.nextInt();
+        System.out.print("Masukkan data 2 : ");
+        b= dataln.nextInt();
+        System.out.print("Masukkan data 3 : ");
+        c= dataln.nextInt();
+        
+        System.out.println("data 1 = "+ a);
+        System.out.println("data 2= "+ b);
+        System.out.println("data 3 = "+ c);
+        rata= (a+b+c)/3;
+        System.out.println("Rata Rata = "+rata );
+
+        
+    }
+    
+}
